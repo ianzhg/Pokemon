9bf47a27ce1a4c080847eaa45d38fb13b887ac42
easyjiajob <55962680+Kennnnn774@users.noreply.github.com>
2023-05-12 17:10:00 -0400

    update us dollar price

--- backend/testshipping.py
+++ backend/testshipping.py
@@ -1,7 +1,11 @@
 from shipping import fedex
+from shipping import japan_post
+from shipping import DHL
+
 from shipping import function_runner
 
 # Call the japan_post function with specified parameters
-result = function_runner(fedex, 1, jp_zipcode="120-0011", us_zipcode="90001")
+# result = function_runner(japan_post, 1, jp_zipcode="120-0011", us_zipcode="90001")
+result = fedex(1, jp_zipcode="120-0011", us_zipcode="90001")
 # Use or print the result as needed
 print(result)
